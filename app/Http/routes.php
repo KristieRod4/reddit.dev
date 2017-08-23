28b8dc1106a85aadc08b8fbabe761ff2886e3b2d
Kristie Rodriguez <pocketkristi@hotmail.com>
2017-08-23 14:52:05 -0500

    Adding routes exercising.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,42 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Create a route at the path /uppercase that takes a parameter that is a word 
+// and returns a string that is the word in all caps.
+
+// returns the sum of the numbers.
+Route::get('/uppercase/{word}', function($word) {
+	return strtoupper($word);
+});
+// Create a route at the path /increment that takes a parameter that is a number 
+// and returns the number plus one.
+Route::get('/increment/{number}', function($number) {
+	if(is_numeric($number)) {
+		return $number + 1;
+	} else {
+		return 1;
+	}
+	
+
+
+});
+
+
+// Bonus
+
+
+
+// Create a route at the path /add that takes two parametes that are numbers and 
+
+Route::get('/add/{num1}/{num2}', function($num1, $num2) {
+	if(is_numeric($num1) && is_numeric($num2)) {
+		return $num1 + $num2;
+	}
+});
+
+
+
+
+
